391e5673413663dc088fe43693c7b6a17e2dbc1b
NicolasJ29 <jeanpierre@lacroixrouge-brest.fr>
2018-11-05 16:05:38 +0100

    Création de l'IHM de base (4 boutons et 2 input text pour les moteurs)

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Ma page web de folie</title>
+	</head>
+	<body>
+		<div style="border:solid;border-radius: 50px;width: 50%;margin-left: 25%;margin-right: 25%;background-color: silver">
+			<center>
+				<h1>Il est beau mon site web</h1>
+			</center>
+		</div>
+		<div>
+			<input type="submit" value="UP">
+			<input type="text" placeholder="Entrer la vitesse M1" width="200px">
+			<input type="submit" value="DOWN">
+			<input type="submit" value="STOP">
+		</div>
+		<div>
+			<input type="submit" value="UP">
+			<input type="text" placeholder="Entrer la vitesse M1" width="200px">
+			<input type="submit" value="DOWN">
+			<input type="submit" value="STOP">
+		</div>
+		</h3>
+	</body>
